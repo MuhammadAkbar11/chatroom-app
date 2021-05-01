2109ddc8628c96316c1e02746b510153105994ad
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-05-01 10:44:08 +0700

    install and setup bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+import { Button, Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <main>
+      <Container>
+        <h1>Hayy</h1>
+      </Container>
+      <Button variant="primary">Hayy</Button>
+    </main>
+  );
+}
+
+export default App;
