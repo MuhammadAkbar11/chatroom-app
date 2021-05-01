7aa90fec670b4f34160035bbe1b7ab83065dcc86
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-05-01 16:38:33 +0700

    create removeUser Helper function

--- server/helper.js
+++ server/helper.js
@@ -14,3 +14,10 @@ export const addUser = ({ socket_id, name, user_id, room_id }) => {
   console.log("user list", users);
   return { user };
 };
+
+export const removeUser = socket_id => {
+  const index = users.findIndex(user => user.socket_id === socket_id);
+  if (index !== -1) {
+    return users.splice(index, 1);
+  }
+};
