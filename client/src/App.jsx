45355e2a6be142a9b0963a0fbc5e5ee9eebc8382
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-05-01 10:51:12 +0700

    Create component and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 import "./App.scss";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Button, Container } from "react-bootstrap";
+import { UserContext } from "./context/UserContext";
+import Home from "./components/Home/Home";
+import Chat from "./components/Chat/Chat";
 
 function App() {
+  const [user, setUser] = useState(null);
+  // const userContext  =UserContext
   return (
-    <main>
-      <Container>
-        <h1>Hayy</h1>
-      </Container>
-      <Button variant="primary">Hayy</Button>
-    </main>
+    <Router>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/chat" component={Chat} />
+        </Switch>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
