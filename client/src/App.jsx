86d9a312a2c05fc2022fb9456153f12720c198cb
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-05-05 11:51:00 +0700

    create route for login and signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { UserContext } from "./context/UserContext";
 
 import Home from "./components/Home/Home";
 import Chat from "./components/Chat/Chat";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/SignUp";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -15,6 +17,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/chat/:room_id/:room_name" component={Chat} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </UserContext.Provider>
     </>
