bf297322279838202dca3e78e3c0b80c317c435d
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-10-07 15:59:50 +0700

    finishing login api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 import asyncHandler from "express-async-handler";
+import { validationResult } from "express-validator";
 import UserModel from "../models/UserModel.js";
 import ErrorResponse from "../utils/errorResponse.js";
+import errorValidation from "../utils/errorValidation.js";
 import generateJwtToken from "../utils/generateJwtToken.js";
 
 export const signup = asyncHandler(async (req, res) => {
@@ -34,12 +36,48 @@ export const signup = asyncHandler(async (req, res) => {
   }
 });
 
-export const login = (req, res) => {
-  res.status(201).json({
-    status: true,
-    message: "login success",
-  });
-};
+export const login = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const errors = validationResult(req);
+
+  const validationMsg = errorValidation(errors.array());
+
+  if (!errors.isEmpty()) {
+    throw new ErrorResponse(
+      403,
+      "ValidationError",
+      "validation Error",
+      validationMsg
+    );
+  }
+
+  try {
+    const user = await UserModel.findOne({ email });
+
+    res.status(201).json({
+      status: true,
+      message: "login success",
+      user: {
+        isAdmin: user.isAdmin,
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateJwtToken(user._id),
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw new ErrorResponse(
+      error.statusCode,
+      error.name,
+      error.message,
+      error.errors
+    );
+  }
+});
 
 export const logout = (req, res) => {
   res.status(201).json({
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -27,6 +27,10 @@ const userSchema = new mongoose.Schema(
 
 userSchema.index({ email: 1, name: 1 }, { unique: true });
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
